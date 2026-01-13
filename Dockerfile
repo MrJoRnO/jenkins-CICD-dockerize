e39d985a69c40149293f21a5f80123d278b3bcc7
Ben-Rafi <jcb31251@nezid.com>
2026-01-13 20:49:41 +0200

    ansible-final1

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,8 @@ WORKDIR /app
 
 COPY . /app
 
+RUN pip install flask
+
 EXPOSE 5000
 
 ENTRYPOINT [ "python", "app.py"]
--- app.py
+++ app.py
@@ -1,7 +1,25 @@
-import time
+# from flask import Flask
+# import os
 
-print("--- האפליקציה התחילה לרוץ בתוך ה-Container! ---")
+# app = Flask(__name__)
 
-while True:
-    print("אני עדיין רץ... הכל עובד מצוין.")
-    time.sleep(5)
+# @app.route('/')
+# def hello():
+#     # המשתנה הזה יעזור לנו במשימה 2 לזהות איזה שירות רץ
+#     service_name = os.getenv('SERVICE_NAME', 'Default Service')
+#     return f"<h1>Hello from {service_name}!</h1><p>Mission 1 is successful!</p>"
+
+# if __name__ == "__main__":
+#     # האזנה ל-0.0.0.0 קריטית כדי שתוכל לגשת מבחוץ דרך דוקר
+#     app.run(host='0.0.0.0', port=5000) 
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "<h1>Mission 1: Success!</h1><p>The Dockerized app is running and accessible.</p>"
+
+if __name__ == "__main__":
+    # האזנה ל-0.0.0.0 קריטית לגישה מחוץ לקונטיינר
+    app.run(host='0.0.0.0', port=5000)
